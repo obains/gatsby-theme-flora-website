f2f13416204af1061fd6e21658c56512d3411856
Oliver Bains <oliver.bains@btinternet.com>
2020-07-19 10:59:57 +0200

    color update

--- src/style/theme.js
+++ src/style/theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export default createMuiTheme({
   palette: {
     primary: {
-      main: "#174141"
+      main: "#2E5489"
     },
     secondary: {
       main: "#000000"
